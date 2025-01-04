daa9fde9b5ff6aa0d8d239730fb519eb004d7372
himanshu.ranjan <himanshu.ranjan@cashfree.com>
2025-01-04 15:31:01 +0530

    sms implementation

--- FactoryDesignPattern/Main.java
+++ FactoryDesignPattern/Main.java
@@ -3,6 +3,8 @@ package FactoryDesignPattern;
 public class Main {
     public  static void main(String [] args) {
         notification whatsapp = new WhatsappFactory().sendNotification();
+        notification sms = new SmsFactory().sendNotification();
         whatsapp.notifi();
+        sms.notifi();
     }
 }
